df96b79cd3885e11ed0a91b29463739da5490a67
CFE <hello@teamcfe.com>
2017-06-24 15:26:03 -0700

    14 - Remembering things with Models

--- src/restaurants/models.py
+++ src/restaurants/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class RestaurantLocation(models.Model):
+    name         = models.CharField(max_length=120)
+    location     = models.CharField(max_length=120, null=True, blank=True)
