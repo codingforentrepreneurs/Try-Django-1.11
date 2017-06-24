47a2ff1af57f3ab4156a5f4142cede762e6d5e4e
CFE <hello@teamcfe.com>
2017-06-24 15:14:01 -0700

    13 - Template View

--- src/muypicky/urls.py
+++ src/muypicky/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
-from restaurants.views import home, about, contact, ContactView
+from restaurants.views import HomeView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', home),
-    url(r'^about/$', about),
-    url(r'^contact/$', ContactView.as_view()),
+    url(r'^$', HomeView.as_view()),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html')),
+    url(r'^contact/$', TemplateView.as_view(template_name='contact.html')),
 ]
--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -2,42 +2,28 @@ import random
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import TemplateView
 # Create your views here.
-# function based view
-def home(request):
-    num = None
-    some_list = [
-        random.randint(0, 100000000), 
-        random.randint(0, 100000000), 
-        random.randint(0, 100000000)
-    ]
-    condition_bool_item = False
-    if condition_bool_item:
-        num = random.randint(0, 100000000)
-    context = {
-        "num": num, 
-        "some_list": some_list
-    }
-    return render(request, "home.html", context)
 
-def about(request):
-    context = {
-    }
-    return render(request, "about.html", context)
+class HomeView(TemplateView):
+    template_name = 'home.html'
 
-
-def contact(request):
-    context = {
-    }
-    return render(request, "contact.html", context)
-
-class ContactView(View):
-    def get(self, request, *args, **kwargs):
-        context = {}
-        return render(request, "contact.html", context)
-
-
-
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        num = None
+        some_list = [
+            random.randint(0, 100000000), 
+            random.randint(0, 100000000), 
+            random.randint(0, 100000000)
+        ]
+        condition_bool_item = True
+        if condition_bool_item:
+            num = random.randint(0, 100000000)
+        context = {
+            "num": num, 
+            "some_list": some_list
+        }
+        return context
 
 
 
