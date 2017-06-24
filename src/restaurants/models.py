841ed24dcc21bef9be86d74d4ec30b8fe42ab376
CFE <hello@teamcfe.com>
2017-06-24 16:52:17 -0700

    20 - SlugField & Unique Slug Generator

--- src/restaurants/models.py
+++ src/restaurants/models.py
@@ -7,6 +7,11 @@ class RestaurantLocation(models.Model):
     category        = models.CharField(max_length=120, null=True, blank=True)
     timestamp       = models.DateTimeField(auto_now_add=True)
     updated         = models.DateTimeField(auto_now=True)
+    slug            = models.SlugField(null=True, blank=True)
     #my_date_field   = models.DateField(auto_now=False, auto_now_add=False)
     def __str__(self):
+        return self.name
+
+    @property
+    def title(self):
         return self.name
