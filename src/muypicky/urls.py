26cc050bc69587622c8311504318f3565bd37e7e
CFE <hello@teamcfe.com>
2017-06-24 16:07:09 -0700

    18 - Generic List View

--- src/muypicky/urls.py
+++ src/muypicky/urls.py
@@ -17,12 +17,17 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
-from restaurants.views import restaurant_listview
+from restaurants.views import (
+    restaurant_listview,
+    RestaurantListView,
 
+)
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='home.html')),
-    url(r'^restaurants/$', restaurant_listview),
+    url(r'^restaurants/$', RestaurantListView.as_view()),
+    url(r'^restaurants/(?P<slug>\w+)/$', RestaurantListView.as_view()),
+    #url(r'^restaurants/asian/$', AsianFusionRestaurantListView.as_view()),
     url(r'^about/$', TemplateView.as_view(template_name='about.html')),
     url(r'^contact/$', TemplateView.as_view(template_name='contact.html')),
 ]
--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,16 +1,30 @@
+from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 from .models import RestaurantLocation
 
-def restaurant_listview(request):
+def restaurant_listview(request,):
     template_name = 'restaurants/restaurants_list.html'
     queryset = RestaurantLocation.objects.all()
     context = {
         "object_list": queryset
     }
     return render(request, template_name, context)
+
+
+class RestaurantListView(ListView):
+    def get_queryset(self):
+        slug = self.kwargs.get("slug")
+        if slug:
+            queryset = RestaurantLocation.objects.filter(
+                    Q(category__iexact=slug) |
+                    Q(category__icontains=slug)
+                )
+        else:
+            queryset = RestaurantLocation.objects.all()
+        return queryset
 
 
