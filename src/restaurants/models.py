76516cd04e670471ed7ef0b4a686ac6e85328153
CFE <hello@teamcfe.com>
2017-06-27 14:43:10 -0700

    28 - Letting Users own Data

--- src/restaurants/models.py
+++ src/restaurants/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.db.models.signals import pre_save, post_save
 
@@ -5,7 +6,10 @@ from django.db.models.signals import pre_save, post_save
 from .utils import unique_slug_generator
 from .validators import validate_category
 
+User = settings.AUTH_USER_MODEL
+
 class RestaurantLocation(models.Model):
+    owner           = models.ForeignKey(User)# Django Models Unleashed JOINCFE.com
     name            = models.CharField(max_length=120)
     location        = models.CharField(max_length=120, null=True, blank=True)
     category        = models.CharField(max_length=120, null=True, blank=True, validators=[validate_category])
