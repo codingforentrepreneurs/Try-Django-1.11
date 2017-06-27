da86b59355f78d19bb7e2af477d47927e89b7f1c
CFE <hello@teamcfe.com>
2017-06-27 15:13:46 -0700

    32 - LoginView

--- src/muypicky/urls.py
+++ src/muypicky/urls.py
@@ -17,6 +17,10 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
+from django.contrib.auth.views import LoginView
+
+
+
 from restaurants.views import (
     restaurant_createview,
     restaurant_listview,
@@ -28,6 +32,7 @@ from restaurants.views import (
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='home.html')),
+    url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^restaurants/$', RestaurantListView.as_view()),
     url(r'^restaurants/create/$',  RestaurantCreateView.as_view()),
     #url(r'^restaurants/create/$',   restaurant_createview ),
